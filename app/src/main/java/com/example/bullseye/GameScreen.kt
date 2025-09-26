ea944e2cefd6112f869dd346c767126ed54240d3
ghores <ghazimoradi.amin@gmail.com>
2025-09-26 13:17:27 +0330

    Implementation build the game ui

---
+++ app/src/main/java/com/example/bullseye/GameScreen.kt
@@ -0,0 +1,45 @@
+package com.example.bullseye
+
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.material3.Button
+import androidx.compose.material3.Slider
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.bullseye.ui.theme.BullseyeTheme
+
+@Composable
+fun GameScreen(modifier: Modifier = Modifier) {
+    Column(
+        modifier = modifier,
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(text = "PUT THE BULLSEYE AS CLOSE AS YOU CAN TO")
+        Text(text = "89", fontSize = 32.sp, fontWeight = FontWeight.Bold)
+        Row {
+            Text(text = "1")
+            Slider(
+                value = 0.5f,
+                onValueChange = {},
+                valueRange = 0.01f..1f
+            )
+            Text(text = "100")
+        }
+        Button(onClick = { }) {
+            Text(text = "HIT ME")
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GameScreenPreview() {
+    BullseyeTheme {
+        GameScreen()
+    }
+}
